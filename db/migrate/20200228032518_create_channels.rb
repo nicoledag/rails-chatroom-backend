3e59c7c993e2359cc1f964890a69c767cb4d56e1
Nicole Dagnenica <nnardella@gmail.com>
2020-02-27 22:25:40 -0500

    Add channel model and table

---
+++ db/migrate/20200228032518_create_channels.rb
@@ -0,0 +1,10 @@
+class CreateChannels < ActiveRecord::Migration[6.0]
+  def change
+    create_table :channels do |t|
+      t.string, :room_name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
