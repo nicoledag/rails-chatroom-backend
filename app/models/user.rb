5c93544ec20e6a9708f63618af1b5442d191b983
Nicole Dagnenica <nnardella@gmail.com>
2020-03-09 11:03:36 -0400

    Add messages and replies dependent destroy, has_secure_password and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-    has_many :messages
-    has_many :replies
+
+    has_secure_password
+
+    has_many :messages, dependent: :destroy
+    has_many :replies, dependent: :destroy
+
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
 end
