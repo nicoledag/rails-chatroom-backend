24f6d9606a4117380d9519777a60988edc8acd81
Nicole Dagnenica <nnardella@gmail.com>
2020-03-23 23:23:20 -0400

    Add logged_in helper method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,8 +5,12 @@ class API::V1::SessionsController < ApplicationController
         User.find_by(id: session[:user_id])
     end
 
-    
-    
+    def logged_in?
+        !!current_user
+    end
+
+    private
+
 
 
 end
